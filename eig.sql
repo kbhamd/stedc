aa489ffa594c6a95ac4b226d10df09b598588e0f
Kjetil Haugen <kjetil.haugen@amd.com>
2023-11-17 17:51:04 -0600

    profile compute performance

--- eig.sql
+++ eig.sql
@@ -11,6 +11,8 @@ SELECT
     count(*) AS calls,
     SUM(grd) AS grid,
     substr(TRIM(kernelname,'"'),1,32) AS kernel,
+    64.0*SUM(2*SQ_INSTS_VALU_FMA_F64 + SQ_INSTS_VALU_MUL_F64+SQ_INSTS_VALU_ADD_F64) AS FLOP,
+    512.0*SUM(SQ_INSTS_VALU_MFMA_MOPS_F64) AS MFMA,
     SUM(FETCH_SIZE)*1024.0/1024/1024 AS RDMB,
     SUM(WRITE_SIZE)*1024.0/1024/1024 AS WRMB,
     SUM(durationNs)*1e-6 AS ms
@@ -28,7 +30,9 @@ SELECT
     PRINTF("%.2e",ms*1e-3) AS sec,
     PRINTF("%7.2f",RDMB/ms) AS RDGBs,
     PRINTF("%7.2f",WRMB/ms) AS WRGBs,
-    PRINTF("%7.2f",(RDMB+WRMB)/ms) AS GBs
+    PRINTF("%7.2f",(RDMB+WRMB)/ms) AS GBs,
+    PRINTF("%7.2f",FLOP/ms*1e-6) AS vGFLOPs,
+    PRINTF("%8.2f",MFMA/ms*1e-6) AS mGFLOPs
 FROM summary
 ORDER BY ms DESC
 ;
