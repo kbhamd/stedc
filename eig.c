973f19baf02a013c1563924f8240c2bfef069d26
Kjetil Haugen <kjetil.haugen@amd.com>
2023-11-17 12:33:38 -0600

    improved timers

--- eig.c
+++ eig.c
@@ -4,8 +4,13 @@
 #include<stdio.h>
 #include<time.h>
 
+
+float clock_delta(struct timespec *y, struct timespec *x){return y->tv_sec-x->tv_sec +1e-9*(y->tv_nsec -x->tv_nsec);}
+
 int main(int argc, const char **argv)
 {
+    struct timespec zero;
+    clock_gettime(CLOCK_MONOTONIC,&zero);
 
     int n=0;
     if(argc==1)
@@ -16,132 +21,131 @@ int main(int argc, const char **argv)
     else
     {
         n=atoi(argv[1]);
-        printf("requested matrix size %d\n",n);
+        printf("requested matrix size %d\n\n",n);
     }
 
-rocblas_handle handle;
-rocblas_create_handle(&handle);
+    rocblas_handle handle;
+    rocblas_create_handle(&handle);
+    rocblas_initialize();
 
-clock_t zero=clock();
-rocblas_initialize();
-clock_t init=clock();
+    struct timespec init;
+    clock_gettime(CLOCK_MONOTONIC,&init);
+    printf("initme %8.4e\n",clock_delta(&init,&zero));
 
-int sweeps=-1; //sweeps syevj only
-double norm=-1.0; //error  syevj only
-double tol=-1.0;
-int info=-1;
-int *INFO;
-int *SWEEPS;
-double *NORM;
-double *TOL;
+    int sweeps=-1; //sweeps syevj only
+    double norm=-1.0; //error  syevj only
+    double tol=-1.0;
+    int info=-1;
+    int *INFO;
+    int *SWEEPS;
+    double *NORM;
+    double *TOL;
 
-//int n= (1<<12);//12800;
-//printf("n=%d\n",n);
-printf("n=%d\n",n);
+    double *a;
+    double *d;
+    double *e;
+    double *A;
+    double *D;
+    double *E;
+    a=malloc(n*n*sizeof(double));
+    d=malloc(n*sizeof(double));
+    e=malloc(n*sizeof(double));
 
-double *a;
-double *d;
-double *e;
-double *A;
-double *D;
-double *E;
-a=malloc(n*n*sizeof(double));
-d=malloc(n*sizeof(double));
-e=malloc(n*sizeof(double));
+    hipMalloc((void **)&A,n*n*sizeof(double));
+    hipMalloc((void **)&D,n*sizeof(double));
+    hipMalloc((void **)&E,n*sizeof(double));
+    hipMalloc((void **)&INFO,sizeof(int));
+    hipMalloc((void **)&SWEEPS,sizeof(int));
+    hipMalloc((void **)&TOL,sizeof(double));
+    hipMalloc((void **)&NORM,sizeof(double));
 
-hipMalloc((void **)&A,n*n*sizeof(double));
-hipMalloc((void **)&D,n*sizeof(double));
-hipMalloc((void **)&E,n*sizeof(double));
-hipMalloc((void **)&INFO,sizeof(int));
-hipMalloc((void **)&SWEEPS,sizeof(int));
-hipMalloc((void **)&TOL,sizeof(double));
-hipMalloc((void **)&NORM,sizeof(double));
+    double MAX=RAND_MAX-1.0;
+    for(int i=0;i<n*n;i++)
+    {
+        //a[i]=1.0;
+        a[i] = rand()/MAX;
+    }
+    for(int i=0;i<n;i++)
+    {
+        d[i]=-1.0;
+    }
+    size_t nbytes=n*n*sizeof(double);
 
-double MAX=RAND_MAX-1.0;
-for(int i=0;i<n*n;i++)
-{
-    //a[i]=1.0;
-    a[i] = rand()/MAX;
-}
-for(int i=0;i<n;i++)
-{
-    d[i]=-1.0;
-}
-size_t nbytes=n*n*sizeof(double);
+    struct timespec memalloc;
+    clock_gettime(CLOCK_MONOTONIC,&memalloc);
+    printf("malloc %8.4e\n",clock_delta(&memalloc,&init));
 
+    hipMemcpy((void*)A,(void*)a,nbytes, hipMemcpyHostToDevice);
+    hipMemcpy((void*)D,(void*)d,n*sizeof(double), hipMemcpyHostToDevice);
+    hipMemcpy((void*)TOL,(void*)&tol,sizeof(double), hipMemcpyHostToDevice);
 
-clock_t memalloc=clock();
-//hipMemcpy((void*)A,(void*)a,sizeof(double)*n,hipMemcpyHtoD);
-hipMemcpy((void*)A,(void*)a,nbytes, hipMemcpyHostToDevice);
-hipMemcpy((void*)D,(void*)d,n*sizeof(double), hipMemcpyHostToDevice);
-hipMemcpy((void*)TOL,(void*)&tol,sizeof(double), hipMemcpyHostToDevice);
+    struct timespec copy2D;
+    clock_gettime(CLOCK_MONOTONIC,&copy2D);
+    printf("copy2D %8.4e\n",clock_delta(&copy2D, &memalloc));
 
-clock_t copy2D=clock();
+    rocsolver_dsyevd(
+        handle,                          // rocblas handle
+        rocblas_evect_original,          // whether or not to compute eigenvectors -> rocblas_evect_original
+        rocblas_fill_upper,              // whether or upper or lower part of matrix is used -> rocblas_fill
+        n,                               // matrix size ,i.e numer of rows/columns of matrix A -> rocblas_int
+        A,                               // real symmetric input matrix -> converted to eignevector matrix at exit
+        n,                               // leading dimension of matrix A -> rocblas_int
+        D,                               // eigenvalues of the matrix A -> pointer to double
+        E,                               // workspace array of dimension n -> pointer to double
+        INFO                             // error code 0=success -> pointer to rocblas_int
+    );
+    /*
+    rocsolver_dsyevj(
+        handle,                          //rocblas handle
+        rocblas_esort_none,              //rocblas_esort_ascending || rocblas_sort_none
+        rocblas_evect_original,          //rocblas_evect_original
+        rocblas_fill_upper,              //rocblas_fill_upper
+        n,                               //matrix dimension
+        A,                               //matrix
+        n,                               //leading matrix dimension == n
+        tol,                             //convergence tolerance -1 implies machine epsilon
+        NORM,                            //error norm -> pointer to double
+        1024,                            //max_sweeps -> maximum number of sweeps
+        SWEEPS,                          //sweeps -> pointer to int
+        D,                               //eigenvalues -> pointer to double of dimension n
+        INFO                             //exit status -> 0 indicates success
+    );
+    */
 
-rocsolver_dsyevd(
-    handle,                          // rocblas handle
-    rocblas_evect_original,          // whether or not to compute eigenvectors -> rocblas_evect_original 
-    rocblas_fill_upper,              // whether or upper or lower part of matrix is used -> rocblas_fill 
-    n,                               // matrix size ,i.e numer of rows/columns of matrix A -> rocblas_int 
-    A,                               // real symmetric input matrix -> converted to eignevector matrix at exit 
-    n,                               // leading dimension of matrix A -> rocblas_int 
-    D,                               // eigenvalues of the matrix A -> pointer to double 
-    E,                               // workspace array of dimension n -> pointer to double  
-    INFO                             // error code 0=success -> pointer to rocblas_int 
-);                     
-/*
-rocsolver_dsyevj(
-    handle,                          //rocblas handle
-    rocblas_esort_none,              //rocblas_esort_ascending || rocblas_sort_none
-    rocblas_evect_original,          //rocblas_evect_original
-    rocblas_fill_upper,              //rocblas_fill_upper
-    n,                               //matrix dimension
-    A,                               //matrix
-    n,                               //leading matrix dimension == n
-    tol,                             //convergence tolerance -1 implies machine epsilon
-    NORM,                            //error norm -> pointer to double
-    1024,                            //max_sweeps -> maximum number of sweeps 
-    SWEEPS,                          //sweeps -> pointer to int
-    D,                               //eigenvalues -> pointer to double of dimension n
-    INFO                             //exit status -> 0 indicates success
-);
-*/
-            
-clock_t dsyevd=clock();
+    struct timespec dsyevd;
+    clock_gettime(CLOCK_MONOTONIC,&dsyevd);
+    printf("dsyevd %8.4e\n",clock_delta(&dsyevd, &copy2D));
 
+    hipMemcpy((void*)d,(void*)D,n*sizeof(double), hipMemcpyDeviceToHost);
+    hipMemcpy((void*)&info,(void*)INFO,sizeof(int), hipMemcpyDeviceToHost);
+    hipMemcpy((void*)&sweeps,(void*)SWEEPS,sizeof(int), hipMemcpyDeviceToHost);
+    hipMemcpy((void*)&norm,(void*)NORM,sizeof(double), hipMemcpyDeviceToHost);
 
-hipMemcpy((void*)d,(void*)D,n*sizeof(double), hipMemcpyDeviceToHost);
-hipMemcpy((void*)&info,(void*)INFO,sizeof(int), hipMemcpyDeviceToHost);
-hipMemcpy((void*)&sweeps,(void*)SWEEPS,sizeof(int), hipMemcpyDeviceToHost);
-hipMemcpy((void*)&norm,(void*)NORM,sizeof(double), hipMemcpyDeviceToHost);
+    struct timespec copy2H;
+    clock_gettime(CLOCK_MONOTONIC,&copy2H);
+    printf("copy2H %8.4e\n",clock_delta(&copy2H,&dsyevd));
 
-clock_t copy2H=clock();
+    free(a);
+    free(d);
+    free(e);
+    hipFree(A);
+    hipFree(D);
+    hipFree(E);
 
-for(int i=0;i<8;i++)
-{
-    printf("%f\n",d[i]);
-}
-printf("%f\n",d[n-1]);
+    rocblas_destroy_handle(handle);
 
-printf("info=%d\n",info);
+    struct timespec memfree;
+    clock_gettime(CLOCK_MONOTONIC,&memfree);
+    printf("freeme %8.4e\n",clock_delta(&memfree, &copy2H));
 
+    printf("total  %8.4e\n",clock_delta(&memfree,&zero));
 
-printf("total =%f\n",(double)(copy2H-zero)/CLOCKS_PER_SEC);
-printf("init  =%f\n",(double)(init-zero)/CLOCKS_PER_SEC);
-printf("alloc =%f\n",(double)(memalloc-init)/CLOCKS_PER_SEC);
-printf("copy2D=%f\n",(double)(copy2D-memalloc)/CLOCKS_PER_SEC);
-printf("syevd =%f\n",(double)(dsyevd-copy2D)/CLOCKS_PER_SEC);
-printf("copy2H=%f\n",(double)(copy2H-dsyevd)/CLOCKS_PER_SEC);
+    printf("\n");
+    for(int i=0;i<8;i++)
+    {
+        printf("%f\n",d[i]);
+    }
+    printf("%f\n",d[n-1]);
 
-free(a);
-free(d);
-free(e);
-hipFree(A);
-hipFree(D);
-hipFree(E);
-
-
-printf("info: %d\n",info);
-rocblas_destroy_handle(handle);
-
+    printf("info=%d\n",info);
 }
